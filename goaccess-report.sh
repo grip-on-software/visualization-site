e9fb838cfdbc749d87c3af4b5bc2f70331d869be
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-07-26 15:56:35 +0200

    Analytics: Real time report using WebSocket
    Proxy the WebSocket connection through caddy and NGINX, with the same
    access restrictions that the HTTP connection does. Add parameters for
    a daemonized version of the report generator.

--- goaccess-report.sh
+++ goaccess-report.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-zcat /var/log/nginx/access.log-*.gz | /usr/local/bin/goaccess -o /srv/goaccess-report/analytics/index.html --log-format='%^[%d:%t %^] "%r" %s %b "%R" "%u" ~h{," }' --date-format '%d/%b/%Y' --time-format '%H:%M:%S' /var/log/nginx/access.log -
+log_path=/var/log/nginx
+params="-o /srv/goaccess-report/analytics/index.html --log-format='%^[%d:%t %^] \"%r\" %s %b \"%R\" \"%u\" ~h{,\" }' --date-format '%d/%b/%Y' --time-format '%H:%M:%S'"
+if [ ! -z "$GOACCESS_DAEMON" ]; then
+	params="$params --origin=http://visualization.gros.example --ws-url=ws.gros.example:80 --addr=127.0.0.1 --real-time-html"
+fi
+
+zcat $log_path/access.log-*.gz | bash -c "/usr/local/bin/goaccess $params $log_path/access.log -"
