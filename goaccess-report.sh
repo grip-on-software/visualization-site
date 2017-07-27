d3d6544c41fff04c04ad7cea9465fdc6446ab9a2
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-07-27 12:19:22 +0200

    Analytics: JSON and query strings for static files
    Consider requests like these to be static files.

--- goaccess-report.sh
+++ goaccess-report.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 log_path=/var/log/nginx
-params="-o /srv/goaccess-report/analytics/index.html --log-format='%^[%d:%t %^] \"%r\" %s %b \"%R\" \"%u\" ~h{,\" }' --date-format '%d/%b/%Y' --time-format '%H:%M:%S'"
+params="-o /srv/goaccess-report/analytics/index.html --log-format='%^[%d:%t %^] \"%r\" %s %b \"%R\" \"%u\" ~h{,\" }' --date-format '%d/%b/%Y' --time-format '%H:%M:%S' --all-static-files --static-file .json"
 if [ ! -z "$GOACCESS_DAEMON" ]; then
 	params="$params --origin=http://visualization.gros.example --ws-url=ws.gros.example:80 --addr=127.0.0.1 --real-time-html"
 fi
