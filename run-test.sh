aa15c02ff32a01e12f3936268556cf7ef7693561
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-07-29 19:43:32 +0200

    Test: Support hosts with old compose versions
    Instead of setting a shm_size, allow the Selenium instance to make use
    of the host's shared memory file.

--- run-test.sh
+++ run-test.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+# Comparable integer format for versions - https://stackoverflow.com/a/37939589
+function version() {
+	echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }';
+}
+
+COMPOSE_VERSION=$(docker-compose version --short)
+COMPOSE_FILES="-f caddy/docker-compose.yml"
+if [ $(version $COMPOSE_VERSION) -lt $(version "1.7.0") ]; then
+	sed -E 's/( +)shm_size: .*/\1volumes:\'$'\n''\1  - \/dev\/shm:\/dev\/shm/' test/docker-compose.yml > test/docker-compose.shm.yml
+	COMPOSE_FILES="$COMPOSE_FILES -f test/docker-compose.shm.yml"
+else
+	COMPOSE_FILES="$COMPOSE_FILES -f test/docker-compose.yml"
+fi
+
 pip install selenium
 
-COMPOSE_FILES="-f caddy/docker-compose.yml -f test/docker-compose.yml"
 PROXY_HOST=nginx
 
 docker-compose $COMPOSE_FILES up -d
