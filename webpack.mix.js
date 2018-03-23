280704a0dd9c502ce5e963cecc4bbac3324cc671
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-03-23 10:43:47 +0100

    Correct resource root for production
    This option is ignored in hot/watch development runs but must be set for
    production assets paths.

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ if (!fs.existsSync(config)) {
 
 Mix.paths.setRootPath(__dirname);
 mix.setPublicPath('www/')
-    .setResourceRoot('')
+    .setResourceRoot('/')
     .js('lib/index.js', 'www/bundle.js')
     .extract(['./lib/build.js'])
     .sass('res/main.scss', 'www/main.css')
