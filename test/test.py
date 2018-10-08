9b09ba18980bdb578660896dd204e187143285a0
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-10-08 16:12:43 +0200

    Test: Increase screen resolution
    Ensure 1024x768 such that Bulma considers us to be a desktop by default.

--- test/test.py
+++ test/test.py
@@ -152,6 +152,8 @@ class IntegrationTest(unittest.TestCase):
         if self._driver is None:
             self.fail('Could not establish Selenium remote driver')
 
+        self._driver.set_window_size(1024, 768)
+
         with open('/config.json') as config_file:
             self._config = json.load(config_file)
 
