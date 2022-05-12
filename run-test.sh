2b035843814413fbfde6a7d09d40d56b5148bb64
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2022-05-12 11:27:36 +0200

    Add LICENSE

--- run-test.sh
+++ run-test.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+# Perform integration tests of visualizations using a virtualized network setup.
+# This script handles the repository updates of the visualizations under test,
+# Docker compose network setup/teardown, additional dependency installation,
+# waiting for builds, logging/publishing results. During the integration tests,
+# code coverage of the JavaScript of the visualizations is tracked and collected
+# into a combined report, Docker/browser logs/screenshots are captured and
+# finally security tests are performed for the dependencies.
+#
+# Copyright 2017-2020 ICTU
+# Copyright 2017-2022 Leiden University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 if [ -z "$VISUALIZATION_SITE_CONFIGURATION" ]; then
     VISUALIZATION_SITE_CONFIGURATION="config.json"
